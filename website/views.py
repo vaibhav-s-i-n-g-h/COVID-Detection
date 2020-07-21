fcc22313c402b377cd79f4da5cd1c86268bdeb6f
vaibhav-s-i-n-g-h <2017csb1117@iitrpr.ac.in>
2020-07-22 00:09:14 +0530

    Added Custom Registration

--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import auth, User
+from django.contrib import messages
+#from .models import User
+
 
 # Create your views here.
 
@@ -8,7 +12,37 @@ def HomePage(request):
     return render(request, 'home.html')
 
 def RegisterPage(request):
-    return render(request, 'register.html')
+
+    if (request.method == 'POST' ):
+
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        age = request.POST['age']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+
+        user = User.objects.create_user(username = email, password = password, first_name= firstname, last_name = lastname, age = age)
+        user.save()
+
+        return redirect('/')
+    
+    else:
+        return render(request, 'register.html')
+
 
 def LoginPage(request):
+
+    if(request.method == 'POST'):
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = email, password=password)
+
+        if(user is not None):
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Email or Password')
+
     return render(request, 'login.html')
