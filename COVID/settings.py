48b50a8bfcee0b4be40de8f08f38030bfa473702
Vaibhav Singh <38386739+vaibhav-s-i-n-g-h@users.noreply.github.com>
2020-08-23 19:54:20 +0530

    Update settings.py

--- COVID/settings.py
+++ COVID/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'COVID.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'COVID19',
-        'USER': 'vaibhav',
-        'PASSWORD': '12345678@$^*Vs',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
